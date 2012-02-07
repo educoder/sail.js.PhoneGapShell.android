89c97cb5b5e806b42b48b35986bc4908e0a6f2ff
Matt Zukowski <matt@zukowski.ca>
2012-02-06 19:17:41 -0500

    app is now automatically reloaded when the sail app url is changed via preferences

--- src/org/educoder/sail/PhoneGapShell.java
+++ src/org/educoder/sail/PhoneGapShell.java
@@ -16,21 +16,14 @@ import com.phonegap.DroidGap;
 
 public class PhoneGapShell extends DroidGap
 {
+	static final int SET_PREFERENCES = 0;
+	
     @Override
     public void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         
-        SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
-        
-        String defaultSailAppUrl = getString(R.string.default_sail_app_url);
-        String appUrl = prefs.getString("sail_app_url", defaultSailAppUrl);
-        if (appUrl.length() == 0) // make sure that the URL isn't blank
-        	appUrl = defaultSailAppUrl;
-        if (appUrl.length() == 0) // make sure that it still isn't blank (in case defaultSailAppUrl was hosed)
-        	appUrl = "file:///android_asset/www/index.html";
-        
-        super.loadUrl(appUrl);
+        super.loadUrl(getAppUrl());
     }
     
     @Override
@@ -49,14 +42,37 @@ public class PhoneGapShell extends DroidGap
     	Log.v(this.getClass().getName(), "Menu item selected: " + item.toString() + " (" + item.getItemId() + ")");
     	if (item.getItemId() == R.id.sail_settings) {
     		Intent sailPrefsActivity = new Intent(getBaseContext(), SailSettings.class);
-    		startActivity(sailPrefsActivity);
+    		startActivityForResult(sailPrefsActivity, PhoneGapShell.SET_PREFERENCES);
     	}
+    }
+    
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == SET_PREFERENCES) {
+            if (resultCode == RESULT_OK) {
+            	super.onActivityResult(requestCode, resultCode, data);
+        		this.loadUrl(this.getAppUrl());
+            }
+        }
     }
     
     public void reload(MenuItem item) {
     	Log.d("PhoneGapShell", "Deleting cache...");
     	this.getCacheDir().delete();
     	this.loadUrl(this.appView.getOriginalUrl());
+    }
+    
+    public String getAppUrl() {
+    	SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
+        
+    	String defaultSailAppUrl = getString(R.string.default_sail_app_url);
+        String appUrl = prefs.getString("sail_app_url", defaultSailAppUrl);
+        if (appUrl.length() == 0) // make sure that the URL isn't blank
+        	appUrl = defaultSailAppUrl;
+        if (appUrl.length() == 0) // make sure that it still isn't blank (in case defaultSailAppUrl was hosed)
+        	appUrl = "file:///android_asset/www/index.html";
+        
+        return appUrl;
     }
 }
 
