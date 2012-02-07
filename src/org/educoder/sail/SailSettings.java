3a2a01916f855dcfc068ec6f2c57f6447590eaeb
Matt Zukowski <matt@zukowski.ca>
2012-02-06 19:17:41 -0500

    app is now automatically reloaded when the sail app url is changed via preferences

--- src/org/educoder/sail/SailSettings.java
+++ src/org/educoder/sail/SailSettings.java
@@ -1,6 +1,7 @@
 package org.educoder.sail;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.preference.Preference;
@@ -14,5 +15,7 @@ public class SailSettings extends PreferenceActivity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		addPreferencesFromResource(R.xml.sail_preferences_screen);
+		setResult(RESULT_OK);
 	}
+
 }
